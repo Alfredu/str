c8f633ab918f5a49df80a9c71f7538cba0d3cf59
Marta Pacuszka <marta.pacuszka@c5s203pc209.fib.upc.es>
2018-03-08 16:46:25 +0100

    my attempt to do distance, does not work obviously

--- distance/distance.ino
+++ distance/distance.ino
@@ -1,5 +1,14 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+boolean measuring_dist = 0;
+float dist = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  setup_timer();
+  setup_interruption();
 
 }
 
@@ -9,16 +18,26 @@ void setup_timer() {
 }
 
 void setup_interruption() {
-  EICRA = _BV(ISC00);
-  EIMSK = _BV(INT0);
+  EICRA = _BV(ISC00); //interrupt triggered by both falling and raising edge
+  EIMSK = _BV(INT0); //we enable interrupts by INT0 (pin21)
   sei();
 }
 
 ISR(INT0_vect) {
   
+  if (!measuring_dist) {
+    /* this part is probably totally wrong. Marta */
+    TCNT1=0;
+    measuring_dist=1;
+  } else {
+    dist=TCNT1/58;
+    measuring_dist=0;
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print(dist);
+  Serial.print("\n");
 
 }
