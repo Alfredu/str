e589243dd0a28283d4efa3ee9d512fa0ef2b5263
Marta Pacuszka <marta.pacuszka@c5s203pc208.fib.upc.es>
2018-03-09 17:54:09 +0100

    lab01 task3, works!

--- distance/distance.ino
+++ distance/distance.ino
@@ -1,6 +1,10 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+/*
+ * This program displays the distance in cm measured with 
+ * the sensor HC-SR04.
+ */
 boolean measuring_dist = 0;
 float dist = 0;
 
@@ -9,35 +13,57 @@ void setup() {
   Serial.begin(9600);
   setup_timer();
   setup_interruption();
+  //we set up PIN22 (PA0) as Trigger 
+  DDRA = _BV(DDA0); //we set PA0 to output mode
+  PORTA = 0;
+  //we set up PIN2 (PE4) as Echo 
+  DDRE &= ~_BV(DDE4); //we set PE4 to input mode
 
 }
 
 void setup_timer() {
-  TCCR1B = _BV(CS11) | _BV(CS10); //we set the prescaler to 64
+  TCCR1A = 0;
+  TCCR1B = _BV(CS11); //we set the prescaler to 8
   
 }
 
 void setup_interruption() {
-  EICRA = _BV(ISC00); //interrupt triggered by both falling and raising edge
-  EIMSK = _BV(INT0); //we enable interrupts by INT0 (pin21)
-  sei();
-}
-
-ISR(INT0_vect) {
-  
-  if (!measuring_dist) {
-    /* this part is probably totally wrong. Marta */
-    TCNT1=0;
-    measuring_dist=1;
-  } else {
-    dist=TCNT1/58;
-    measuring_dist=0;
-  }
+  cli();
+  EICRB = _BV(ISC40); //interrupt triggered by both falling and raising edge
+  EIMSK = _BV(INT4); //we enable interrupts by INT4 (pin2)
+  sei(); //enable global interrupts
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print(dist);
-  Serial.print("\n");
-
+  //We generate high signal for trigger for >10ms so that 
+  //the sensor would generate echo signal that will enable
+  //an interruption
+  PORTA |= _BV(PA0);
+  _delay_ms(20);
+  PORTA &= ~_BV(PA0);
+  _delay_ms(50);
+  Serial.println(dist);
 }
+
+
+
+ISR(INT4_vect) {
+  
+  if (!measuring_dist) {
+    TCCR1B = 0;
+    TCNT1=0;
+    TCCR1B = _BV(CS11); 
+    measuring_dist=1;
+  } else {
+    unsigned int useconds = TCNT1*0.5f;
+    dist=(float)(useconds)/58.0f;
+    measuring_dist=0;
+
+    
+  }
+}
+
+
+
+
+
